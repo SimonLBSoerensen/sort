f4cc1650b534d03a0eb1e2f2d4cabe6b7a61779a
SimonLBS <simonlyckbjaert@hotmail.com>
2021-03-29 11:42:27 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -196,7 +196,7 @@ def associate_detections_to_trackers(detections,trackers,iou_threshold = 0.3):
 
 
 class Sort(object):
-  def __init__(self, max_age=1, min_hits=3, iou_threshold=0.3):
+  def __init__(self, max_age, min_hits, iou_threshold):
     """
     Sets key parameters for SORT
     """
